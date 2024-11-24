196dc4859a316e7825b0ee919795498781c6b926
zzz <zzz@qq.com>
2024-11-24 16:44:25 +0800

    支持退订

--- projects/poem-email/src/helpers/mod.rs
+++ projects/poem-email/src/helpers/mod.rs
@@ -11,7 +11,7 @@ pub trait EmailSender {
 
     fn company_name(&self) -> Cow<str>;
     fn sender_mail(&self) -> Mailbox;
-    fn login_code(&self, receiver: &str, code: &str) -> Result<Response, Error> {
+    fn login_code(&self, receiver: &str, code: &str, unsubscribe: &str) -> Result<Response, Error> {
         let subject = match self.company_name().as_ref() {
             "" => "登录验证".to_string(),
             s => format!("【{}】 登录验证", s),
@@ -24,7 +24,11 @@ pub trait EmailSender {
             .subject(subject)
             .header(ContentType::TEXT_HTML)
             .body(format!(
-                "您的动态验证码为：{code}，您正在进行邮箱登录，此验证码五分钟内有效。如非本人操作，请忽略本邮件！"
+                r#"
+<p>您的动态验证码为：{code}，您正在进行邮箱登录，此验证码五分钟内有效。</p>
+<p>如非本人操作，请忽略本邮件。</p>
+<p>退订链接: {unsubscribe} (退订后您将无法使用邮箱登录或注册!)</p>
+"#
             ))?;
         self.send_message(&message)
     }
