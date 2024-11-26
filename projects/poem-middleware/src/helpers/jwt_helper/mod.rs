161df99d04d817bd0eb1b32dd70e211ba00ef440
zzz <zzz@qq.com>
2024-11-26 19:11:36 +0800

    支持 jwt

--- projects/poem-middleware/src/helpers/jwt_helper/mod.rs
+++ projects/poem-middleware/src/helpers/jwt_helper/mod.rs
@@ -1,6 +1,10 @@
 use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Validation};
-use poem::http::StatusCode;
+use poem::{Request, http::StatusCode};
 use serde::{Serialize, de::DeserializeOwned};
+use std::{
+    ops::Add,
+    time::{Duration, SystemTimeError},
+};
 
 pub fn jwt_encode<T: Serialize>(claims: T, private_key: &[u8]) -> poem::Result<String> {
     let header = jsonwebtoken::Header::new(Algorithm::ES256);
@@ -12,10 +16,25 @@ pub fn jwt_encode<T: Serialize>(claims: T, private_key: &[u8]) -> poem::Result<S
 }
 
 pub fn jwt_decode<T: DeserializeOwned>(token: &str, public_key: &[u8]) -> poem::Result<T> {
-    let method = Validation::new(Algorithm::ES256);
+    let mut method = Validation::new(Algorithm::ES256);
+    method.validate_exp = true;
+    method.validate_aud = false;
     let key = DecodingKey::from_ec_pem(public_key).expect("非法解密密钥");
     match jsonwebtoken::decode::<T>(&token, &key, &method) {
         Ok(o) => Ok(o.claims),
         Err(e) => Err(poem::Error::from_string(format!("非法访问: {}", e), StatusCode::UNAUTHORIZED)),
+    }
+}
+pub fn jwt_request<T: DeserializeOwned>(input: &Request, public_key: &[u8]) -> poem::Result<T> {
+    match input.header("Authorization") {
+        Some(s) => Ok(jwt_decode(s.trim_start_matches("Bearer "), public_key)),
+        None => Err(poem::Error::from_string("非法访问: 缺少 `Authorization`", StatusCode::UNAUTHORIZED)),
+    }
+}
+
+pub fn jwt_time(time: f32) -> poem::Result<u64> {
+    match std::time::SystemTime::now().add(Duration::from_secs_f32(time)).duration_since(std::time::UNIX_EPOCH) {
+        Ok(o) => Ok(o.as_secs()),
+        Err(_) => Err(poem::Error::from_string("非法时钟".to_string(), StatusCode::CONFLICT)),
     }
 }
--- projects/poem-middleware/src/helpers/mod.rs
+++ projects/poem-middleware/src/helpers/mod.rs
@@ -1,3 +1,3 @@
 mod jwt_helper;
 
-pub use self::jwt_helper::{jwt_decode, jwt_encode};
+pub use self::jwt_helper::{jwt_decode, jwt_encode, jwt_request, jwt_time};
