7bc2ac05030287d01b1fb28ecf2813f46171c1ea
zzz <zzz@qq.com>
2024-12-13 17:05:23 +0800

    移除 orm

--- projects/semantic-id/src/helpers/mod.rs
+++ projects/semantic-id/src/helpers/mod.rs
@@ -1,9 +1,3 @@
-#[cfg(feature = "sea-orm")]
-use sea_orm::EntityTrait;
-
 pub trait SemanticKey: Send + Sync {
     const KEY: &'static str;
-
-    #[cfg(feature = "sea-orm")]
-    type Entity: EntityTrait;
 }
