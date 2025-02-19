b612b64e349622faa2f693f217bd349271e109e6
zzz <zzz@qq.com>
2025-02-19 23:55:11 +0800

    编码错误

--- projects/poem-middleware/src/errors/for_sqlx.rs
+++ projects/poem-middleware/src/errors/for_sqlx.rs
@@ -5,6 +5,8 @@ impl From<Error> for YxError {
     fn from(value: Error) -> Self {
         let kind = match &value {
             Error::Database(e) => YxErrorKind::DatabaseError { message: e.to_string() },
+            Error::Encode(e) => YxErrorKind::EncodeError { format: "".to_string(), message: e.to_string() },
+            Error::Decode(e) => YxErrorKind::DecodeError { format: "".to_string(), message: e.to_string() },
             _ => YxErrorKind::DatabaseError { message: value.to_string() },
         };
         Self { kind: Box::new(kind) }
